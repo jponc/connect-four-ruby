613cf4f68707bedcdcb8d62e94c2594d884483b8
Julian Ponce <hello@julianponce.com>
2018-08-13 22:23:25 +0800

    Added vertical check

--- state_checker.rb
+++ state_checker.rb
@@ -1,15 +1,44 @@
 class StateChecker
   attr_reader :state,
               :row,
-              :column
+              :column,
+              :rows_count,
+              :columns_count,
+              :value
 
   def initialize(options)
     @state = options[:state]
+
     @row = options[:row]
     @column = options[:column]
+    @value = @state[@row][@column]
+
+    @rows_count = @state.count
+    @columns_count = @state.first.count
   end
 
   def win?
+    vertical? || horizontal? || diagonal?
+  end
+
+  def vertical?
+    last_row = row + 3
+
+    return false if last_row >= rows_count
+
+    cell_values =
+      (row..last_row).to_a.map do |r|
+        state[r][column]
+      end
+
+    cell_values.all?(value)
+  end
+
+  def horizontal?
+    false
+  end
+
+  def diagonal?
     false
   end
 end
