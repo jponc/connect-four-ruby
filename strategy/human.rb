87b1e96e1b27c2fa87088b9e700e4b6fe8a37a62
Julian Ponce <hello@julianponce.com>
2018-08-13 21:59:40 +0800

    Added a way to retry human input

--- strategy/human.rb
+++ strategy/human.rb
@@ -1,9 +1,21 @@
 module Strategy
   module Human
     def self.select(board)
-      valid_columns = board.valid_columns
-      print "Select column #{valid_columns}: "
-      gets.chomp.to_i
+      loop do
+        valid_columns = board.valid_columns.map(&:to_s)
+
+        puts "Valid Columns: #{valid_columns}"
+        print "Selected: "
+        selected = gets.chomp
+
+        return selected.to_i if is_valid?(valid_columns, selected)
+
+        puts 'You selected an invalid column. Try again.'
+      end
+    end
+
+    def self.is_valid?(valid_columns, selected)
+      valid_columns.include?(selected)
     end
   end
 end
